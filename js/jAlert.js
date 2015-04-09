512ea0a4f56d42eb7a931e229ebe085eb28ca45a
LekaNukem <jussilait92@gmail.com>
2015-04-09 14:54:11 +0300

    CSS Animations, Top, Middle, Bottom
    More options, including use of CSS Animation rather than just rapidly
    changing values with jQuery's animate function.

--- js/jAlert.js
+++ js/jAlert.js
@@ -16,26 +16,64 @@ jAlert = function(msg, options) {
 }
 
 jAlert.display = function(msg, options) {
-    //Is time parameter set?
-    //if (time === undefined || typeof time !== 'number')
-    //{
-    //    time = 2000;
-    //}
+    //Option variables
+    var time = 2000;
+    var vAlign = 'bottom';
+    var useCssAnimation = false;
 
     //Check options
     if (options !== undefined || typeof options !== object)
     {
+        if(options.time !== undefined)
+        {
+            time = options.time;
+        }
 
+        if(options.vAlign !== undefined)
+        {
+            var temp = options.vAlign;
+            if (temp === 'bottom' || temp === 'middle' || temp === 'top')
+            {
+                vAlign = options.vAlign;
+            }
+            else
+            {
+                console.log('Warning: options.vAlign value must be either "bottom", "middle" or "top". Falling back to default value');
+            }
+        }
+
+        if (options.useCssAnimation !== undefined)
+        {
+            if (options.useCssAnimation === true || options.useCssAnimation === false)
+            useCssAnimation = options.useCssAnimation;
+        }
     }
 
     //Create container for the info message
     var $container = $('<div></div>');
     $container.append('<span id="jAlert_msg">' + msg + '</span>');
 
+    //Check where to position the popup vertically. f stands for flipper (for bottom start/end to *-1)
+    var bottomPx
+    var f = 1;
+    if (vAlign === 'bottom')
+    {
+        bottomPx = 40;
+        f = -1;
+    }
+    else if (vAlign === 'middle')
+    {
+        bottomPx = ( $(window).height() / 2 );
+    }
+    else
+    {
+        bottomPx = ( $(window).height() - 80 );
+    }
+
     //Make CSS Declarations
     $container.css({
         position: 'fixed',
-        bottom: '0px',
+        bottom: bottomPx + 'px',
         //right: '30px',
         marginLeft: 'auto',
         marginRight: 'auto',
@@ -53,6 +91,9 @@ jAlert.display = function(msg, options) {
     $container.find('span').css({
         color: '#ffffff',
         textShadow: '0px 0px 6px #000000',
+        fontWeight: 'bold',
+        fontSize: '1.0em',
+        fontFamily: 'Segoe UI, Roboto, Verdana',
         wordWrap: 'break-word'
     });
 
@@ -64,22 +105,55 @@ jAlert.display = function(msg, options) {
     $('body').append($container);
 
     //Position the just added container NOW because earlier it would give wrong width
-    var posLeft = ($('body').width() / 2) - ( $container.outerWidth() / 2 );
+    var posLeft = ($(window).width() / 2) - ( $container.outerWidth() / 2 );
     $container.css( { left: posLeft } );
 
-    //Animate the container
-    $container.animate({
-        opacity: 1.0,
-        bottom: '40px'
-    }, 750, 'swing', function() {
-        setTimeout(function() {
-            $($container).animate({
-                opacity: 0.0,
-                bottom: '80px'
-            }, 750, 'swing', function() {
-                //Remove the container
+    //Use CSS or jQuery animations
+    if(!useCssAnimation) {
+        //Animate the container
+        $container.animate({
+            opacity: 1.0,
+            bottom: (bottomPx - (40 * f)) + 'px'
+        }, 750, 'swing', function () {
+            setTimeout(function () {
+                $($container).animate({
+                    opacity: 0.0,
+                    bottom: (bottomPx - (80 * f)) + 'px'
+                }, 750, 'swing', function () {
+                    //Remove the container
+                    $container.remove();
+                });
+            }, time);
+        });
+    }
+    else {
+        var transitionEndCounter = 0;
+        $container.css({
+            transition: 'all 750ms ease-out'
+        });
+
+        $($container).bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+            console.log(transitionEndCounter);
+            if (transitionEndCounter == 0)
+            {
+                window.setTimeout(function() {
+                    $container.css({
+                        opacity: 0.0,
+                        bottom: (bottomPx - (80 * f)) + 'px'
+                    });
+                    transitionEndCounter++;
+                }, time);
+            }
+            else
+            {
                 $container.remove();
-            });
-        }, time);
-    });
+            }
+        });
+
+        //Change some bottom, opacity CSS properties
+        $container.css({
+            opacity: 1.0,
+            bottom: (bottomPx - (40 * f)) + 'px'
+        });
+    }
 };
